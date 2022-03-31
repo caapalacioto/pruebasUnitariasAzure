0437b8be6ab1b3741ba8c6d7ae8e7de9ed44d6d8
Carlos Andrés Palacio <caapalacioto@unal.edu.co>
2022-03-31 14:16:57 -0500

    se crea la funcion de suma y se realizan las pruebas de suma de cadenas, numero enteros y flotantes

---
+++ test.py
@@ -0,0 +1,16 @@
+import suma
+import unittest
+
+class TestMyModule(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(suma.sum(5, 7), 12)
+    def test_sum_negative(self):
+        self.assertEqual(suma.sum(-5, -7), -12)
+    def test_sum_cero(self):
+        self.assertEqual(suma.sum(0, 0), 0)
+    def test_sum_strin(self):
+        self.assertRaises(TypeError, suma.sum, 0, 'a')
+
+
+if __name__ == '__main__':
+    unittest.main()
