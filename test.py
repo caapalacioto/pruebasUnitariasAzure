3ffbf7e98828295fb08e75db44c82568f68c1af4
Carlos Andrés Palacio Tobón <98239666+caapalacioto@users.noreply.github.com>
2022-04-05 15:37:41 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestMyModule(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(suma.sum(5, 7), 12)
     def test_sum_negative(self):
-        self.assertEqual(suma.sum(-5, -7), 12)
+        self.assertEqual(suma.sum(-5, -7), -12)
     def test_sum_cero(self):
         self.assertEqual(suma.sum(0, 0), 0)
     def test_sum_strin(self):
